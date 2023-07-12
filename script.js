2976a8b7223253a8961e36c7b8dcdef68d73c654
Andrew Erickson <andrewerickson@MacBook-Air.local>
2023-07-12 17:11:14 -0500

    timer-start button

--- script.js
+++ script.js
@@ -3,3 +3,19 @@
 var instructions = document.getElementById("multiple-choice")
 
 instructions.style.color = "gold"
+
+
+
+var possibleAnswers = document.getElementsByClassName("choices")
+
+//querySelectorAll(possibleAnswers)
+
+//console.log(possibleAnswers)
+var question = document.getElementById("question-content")
+
+function firstQuestion(){
+    question.textContent = "this is the first question"
+    return textContent
+}
+
+console.log(document.body)
